3d6b770fa04cd69e75a629a2f186568e8eaeb429
Greg Miller <hello@gregmiller.io>
2023-11-06 17:54:22 -0500

    Setup project files
    - Add acf-fields.json
    - acf-fields renders block.json
    - Add info to other files about the purpose

--- kks-newsletter.php
+++ kks-newsletter.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       KKS
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       KKS Newsletter
+ * Description:       Displays Mad Mimi Newsletters via iframe
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Shrinkray
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       kks-newsletter
--- readme.txt
+++ readme.txt
@@ -1,12 +1,12 @@
 === KKS ===
-Contributors:      The WordPress Contributors
+Contributors:      Shrinkray
 Tags:              block
 Tested up to:      6.1
 Stable tag:        0.1.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
-Example block scaffolded with Create Block tool.
+Block displays Mad Mimi newsletters via iframe.
 
 == Description ==
 
