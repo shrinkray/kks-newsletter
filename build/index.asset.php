d7814214427e6d66946bbe958cf0148b77c6e614
Greg Miller <hello@gregmiller.io>
2023-11-07 18:06:44 -0500

    Remove from project

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5f164b4512099e5c9126');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '65a2fb2f7172432c9585');
